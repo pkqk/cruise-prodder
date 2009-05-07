807564846eeab4009f93186f12b5acc3f501c41e
Adam Johnson <adam@pkqk.net>
2009-05-07 11:14:30 +0100

    log trigger event

--- lib/cruise_prodder.rb
+++ lib/cruise_prodder.rb
@@ -12,6 +12,7 @@ class CruiseProdder
   def build_fixed(build, previous_build)
     if server and project
       Net::HTTP.new(server.to_s, port).post("/projects/build/#{project}",'')
+      CruiseControl::Log.event("Triggering build of #{project} on #{server}:#{port}"
     end
   end
   
