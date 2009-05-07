895fe5c45418cf5260ecf528f106f0bb6a8b23e0
Adam Johnson <adam@pkqk.net>
2009-05-07 12:03:57 +0100

    notify more than one project

--- lib/cruise_prodder.rb
+++ lib/cruise_prodder.rb
@@ -1,19 +1,26 @@
 class CruiseProdder
+  
+  class Proddee
+    def initialize(server, project, port)
+      @server, @project, @port = server, project, port
+    end
 
-  attr_accessor :server, :project
-  attr_writer :port
-  def port
-    @port || 3333
+    def trigger
+      Net::HTTP.new(@server.to_s, @port).post("/projects/build/#{@project}",'')
+    end
   end
 
   def initialize(project = nil)
+    @proddees = []
+  end
+  
+  def notify(server, project, port=3333)
+    @proddees << Proddee.new(server, project, port)
   end
   
   def build_finished(build)
     return if build.failed?
-    if server and project
-      Net::HTTP.new(server.to_s, port).post("/projects/build/#{project}",'')
-    end
+    @proddees.each { |proddee| proddee.trigger }
   end
 
 end
